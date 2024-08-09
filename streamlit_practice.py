46513fd805887f683e6d803055419270e7ecf167
sidd1398 <useful0220@naver.com>
2024-08-09 23:46:53 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -13,10 +13,10 @@ left = st.text_input("왼쪽 우파루:")
 right = st.text_input("오른쪽 우파루:")
 
 # pandas를 사용해 csv 파일 읽기
-#df = pd.read_csv('streamlit.csv')
+df = pd.read_csv('streamlit.csv')
 
 if st.button("크로스"):
-#    st.write(df)  # DataFrame 전체를 출력
+    st.write(df)  # DataFrame 전체를 출력
     st.write('ddd')
 
 
