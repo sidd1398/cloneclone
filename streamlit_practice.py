a86a6f76a6965f50b6bda61f562519bea0b77ef4
sidd1398 <useful0220@naver.com>
2024-08-10 00:56:06 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -14,7 +14,8 @@ st.cache_data.clear()
 try:
     name_data = pd.read_csv("wooparoo_list_data.csv", encoding="utf-8")
     compressed_data = pd.read_csv("wooparoo_all_data_compressed.csv", encoding="utf-8")
-    st.write("파일 로드 성공")
+    st.write("Update: 2024-08-10")
+    st.write("Made By 시드드#0001")
 except Exception as e:
     st.error(f"파일 로드 실패: {e}")
     st.stop()
@@ -49,7 +50,7 @@ if st.button("버튼을 누르면 결과창이 출력됩니다."):
     right_sno = name_to_sno(right_name, name_to_sno_dict)
 
     if left_sno is None or right_sno is None:
-        st.error("One or both names were not found in the list.")
+        st.error("입력하신 이름의 우파루가 존재하지 않습니다.")
     else:
         # wooparoo_all_data_sorted.csv 파일에서 [left, right]와 일치하는 [result, rate] 찾기
         with open("wooparoo_all_data_compressed.csv", "r", encoding="utf-8") as sorted_file:
@@ -88,7 +89,7 @@ if st.button("버튼을 누르면 결과창이 출력됩니다."):
                 df.index = df.index + 1  # 행 번호를 1부터 시작하도록 설정
                 st.table(df)
             else:
-                st.error(f"No matching result found for {left_name} (left) and {right_name} (right).")
+                st.error(f"파일에 우파루 조합이 존재하지 않습니다. {left_name} (left), {right_name} (right).")
 
 
 # In[ ]:
