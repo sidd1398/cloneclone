b1dc373cd78034562b30533bdb4bd85ee201a88d
sidd1398 <useful0220@naver.com>
2024-12-01 14:16:26 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -563,6 +563,7 @@ elif option == "농장 생산량 메모":
     ]
     
     framed_farm_data = pd.DataFrame(farm_data[1:], columns=farm_data[0])
+    framed_farm_data.index = framed_farm_data.index * 0
     styled_farm_data = framed_farm_data.style.set_properties(**{'text-align': 'right'})
     
     st.table(styled_farm_data)
