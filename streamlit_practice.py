33e849aee419839bed25513c3eb47f3bb027a83b
sidd1398 <useful0220@naver.com>
2025-01-08 11:10:10 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -23,8 +23,8 @@ import pandas as pd
 
 st.cache_data.clear()
 
-st.write("안내사항: 아티오네 확률업 업데이트")
-st.write("Update: 2025-01-04")
+st.write("안내사항: 쿠키자루 업데이트")
+st.write("Update: 2025-01-08")
 st.write("Made by 시드드#0001")
 st.write("Thanks to kjeok00, replica, yskunn")
 
