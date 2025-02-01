7e794c67ba2f8e81ee70d1c162f9fca589139c47
sidd1398 <useful0220@naver.com>
2025-02-01 10:14:04 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -23,9 +23,9 @@ import pandas as pd
 
 st.cache_data.clear()
 
-st.write("안내사항: 까미 업데이트")
+st.write("안내사항: 플로디, 플로디네 업데이트")
 st.write("기준: 유저레벨 30 이상, 우파루 레벨 16 이상")
-st.write("Update: 2025-01-28")
+st.write("Update: 2025-02-01")
 st.write("Made by 시드드#0001")
 st.write("Thanks to kjeok00, replica, yskunn")
 
