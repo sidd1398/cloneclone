dd562e8906fb49db4f38238797f60868911df79d
sidd1398 <useful0220@naver.com>
2025-03-08 10:15:47 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -23,7 +23,10 @@ import pandas as pd
 
 st.cache_data.clear()
 
-st.write("안내사항: 허니럽, 캔디럽 업데이트")
+st.write("안내사항: 슈루루 확률업 업데이트")
+st.write("슈루루 확률업: 03.08. 09:00 ~ 03.09. 08:59")
+st.write("샤벨 확률업: 03.08 18:00 ~ 03.08. 23:59")
+st.write("샤벨의 확률업 수치는 따로 업데이트하지 않을 예정입니다")
 st.write("기준: 유저레벨 30 이상, 우파루 레벨 16 이상")
 st.write("Update: 2025-03-05")
 st.write("Made by 시드드#0001")
