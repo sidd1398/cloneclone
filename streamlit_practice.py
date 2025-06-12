5eeb3b060af05d140f39b3013a7320509924a25e
sidd1398 <useful0220@naver.com>
2025-06-12 22:16:14 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -26,9 +26,9 @@ st.cache_data.clear()
 
 #st.write("당분간 가상크로스, 다중조합찾기 모드를 중단합니다 ㅜㅜ")
 #st.write(" ")
-st.write("안내사항: 햄찌볼 업데이트")
+st.write("안내사항: 6/11 업데이트 반영")
 st.write("기준: 유저레벨 30 이상, 우파루 레벨 16 이상")
-st.write("Update: 2025-05-31")
+st.write("Update: 2025-06-12")
 st.write("Made by 시드드#0001")
 st.write("Thanks to kjeok00, replica, yskunn")
 
