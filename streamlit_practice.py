e462d08768f828745e066a7967ecfa26007f8e9c
sidd1398 <useful0220@naver.com>
2025-09-22 21:31:24 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -25,9 +25,9 @@ import glob
 st.cache_data.clear()
 
 # Streamlit 사용자 인터페이스
-st.write("안내사항: 요호, 티파니 업데이트")
+st.write("안내사항: 로롤리 업데이트")
 st.write("기준: 유저레벨 30 이상, 우파루 레벨 16 이상")
-st.write("Update: 2025-08-10")
+st.write("Update: 2025-09-22")
 st.write("Made by 시드드#0001")
 st.write("Thanks to kjeok00, replica, yskunn")
 
