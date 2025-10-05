287de0edcbd5ef809c0fb1d73aaede77b9c0cd69
sidd1398 <useful0220@naver.com>
2025-10-05 10:37:07 +0900

    Add files via upload

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -25,9 +25,9 @@ import glob
 st.cache_data.clear()
 
 # Streamlit 사용자 인터페이스
-st.write("안내사항: 앵두 확률업")
+st.write("안내사항: 해치루, 해태루, 코아타, 로롤리, 블라이 확률업")
 st.write("기준: 유저레벨 30 이상, 우파루 레벨 16 이상")
-st.write("Update: 2025-10-04")
+st.write("Update: 2025-10-05")
 st.write("Made by 시드드#0001")
 st.write("Thanks to kjeok00, replica, yskunn")
 
